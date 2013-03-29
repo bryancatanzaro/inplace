febac0d645e0746999b388a03be2fce2f23df5d1
Bryan Catanzaro <bcatanzaro@acm.org>
2013-03-29 15:49:32 -0700

    Fixes to coarse rotate.

--- rotate.cu
+++ rotate.cu
@@ -3,12 +3,64 @@
 #include <thrust/device_vector.h>
 #include <iostream>
 
+template<typename T, typename Fn>
+void print_array(const thrust::device_vector<T>& d, Fn index) {
+    int m = index.m_m;
+    int n = index.m_n;
+    thrust::host_vector<T> h = d;
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            T x = h[index(i, j)];
+            std::cout.width(3); std::cout << std::right;
+            std::cout << x << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+
 int main() {
     int m = 512;
-    int n = 16000;
-    thrust::device_vector<float> x(m * n);
+    int n = 32000;
+    // int m = 6;
+    // int n = 35;
+    thrust::device_vector<int> x(m * n);
+    thrust::copy(thrust::counting_iterator<int>(0),
+                 thrust::counting_iterator<int>(0) + m * n,
+                 x.begin());
+
+    // print_array(x, inplace::row_major_index(m, n));
+    std::cout << std::endl;
+
+    int block_size = 256;
+    int n_blocks = (n-1)/block_size + 1;
+
+    std::cout << "m: " << m << " n: " << n;
+    std::cout << " n_blocks: " << n_blocks << " block_size: " << block_size;
+    std::cout << std::endl;
     
-    inplace::coarse_col_rotate<<<(n-1)/256+1, 256>>>(
+    cudaEvent_t start,stop;
+    float time=0;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+    cudaEventRecord(start, 0);
+
+    
+
+    
+    inplace::coarse_col_rotate<<<n_blocks, block_size>>>(
         m, n, thrust::raw_pointer_cast(x.data()));
    
+    
+    cudaEventRecord(stop, 0);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&time, start, stop);
+    
+    std::cout << "  Time: " << time << " ms" << std::endl;
+    float gbs = (float)(m * n * sizeof(float) * 2) / (time * 1000000);
+    std::cout << "  Throughput: " << gbs << " GB/s" << std::endl;
+
+
+    // print_array(x, inplace::row_major_index(m, n));
+    
 }
--- rotate.h
+++ rotate.h
@@ -2,55 +2,66 @@
 #include "index.h"
 namespace inplace {
 
-__device__ int gcd(int a, int b) {
-    int x = 0;
-    int last_x = 1;
-    int y = 1;
-    int last_y = 0;
-    while (b > 0) {
-        int quotient = a / b;
-        int new_b = a % b;
-        a = b;
-        b = new_b;
-        int new_x = last_x - quotient * x;
-        last_x = x;
-        x = new_x;
-        int new_y = last_y - quotient * y;
-        last_y = y;
-        y = new_y;
+__device__ __forceinline__
+unsigned int ctz(unsigned int x) {
+    return __ffs(x) - 1;
+}
+
+__device__ __forceinline__
+unsigned int gcd(unsigned int x, unsigned int y) {
+    if (x == 0) return y;
+    if (y == 0) return x;
+    unsigned int cf2 = ctz(x | y);
+    x >>= ctz(x);
+    while (true) {
+        y >>= ctz(y);
+        if (x == y) break;
+        if (x > y) {
+            unsigned int t = x; x = y; y = t;
+        }
+        if (x == 1) break;
+        y -= x;
     }
-    return a;
+    return x << cf2;
+}
+
+__device__ __forceinline__
+unsigned int div_up(unsigned int a, unsigned int b) {
+    return (a-1)/b + 1;
+}
+
+__device__ __forceinline__
+unsigned int div_down(unsigned int a, unsigned int b) {
+    return a / b;
 }
 
 template<typename T>
 __global__ void coarse_col_rotate(int m, int n, T* d) {
-
+    int warp_id = threadIdx.x & 0x1f;
     int global_index = threadIdx.x + blockIdx.x * blockDim.x;
-    int rotation_amount = global_index % m;
-
-    int approximator = 32;
-    int coarse_rotation = rotation_amount & (~(approximator-1));
+    int rotation_amount = (global_index - warp_id) % m;
+    int col = global_index;
     
-    if ((global_index < n) && (coarse_rotation > 0)) {    
+    if ((col < n) && (rotation_amount > 0)) {
         row_major_index rm(m, n);
-        int c = gcd(coarse_rotation, m);
-        int inc = m - coarse_rotation;
+        int c = gcd(rotation_amount, m);
+        int inc = m - rotation_amount;
         for(int b = 0; b < c; b++) {
             int pos = b;
-            T prior = d[rm(pos, global_index)];
+            T prior = d[rm(pos, col)];
             int next_pos = pos + inc;
             if (next_pos >= m)
                 next_pos -= m;
             while (next_pos >= c) {
-                T temp = d[rm(next_pos, global_index)];
+                T temp = d[rm(next_pos, col)];
                 pos = next_pos;
-                d[rm(pos, global_index)] = prior;
+                d[rm(pos, col)] = prior;
                 prior = temp;
-                next_pos += inc;
+                next_pos = pos + inc;
                 if (next_pos >= m)
                     next_pos -= m;
             }
-            d[rm(next_pos, global_index)] = prior;
+            d[rm(next_pos, col)] = prior;
 
         }
     }
