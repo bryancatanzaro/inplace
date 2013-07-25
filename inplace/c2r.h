38b1ae2f1b69324c012f0d91f976e46b4b755dc5
Bryan Catanzaro <bcatanzaro@acm.org>
2013-07-24 18:34:20 -0700

    Rollback reductions to help isolate issue.

--- inplace/c2r.h
+++ inplace/c2r.h
@@ -43,12 +43,14 @@ struct postpermuter {
 
 struct shuffle {
     int m, n, k;
-    reduced_divisor_32 b;
-    reduced_divisor_32 c;
-    reduced_divisor_64 c_64;
+    int b, c;
+  //   reduced_divisor_32 b;
+//     reduced_divisor_32 c;
+// //
+    //reduced_divisor_64 c_64;
     __host__
     shuffle(int _m, int _n, int _c, int _k) : m(_m), n(_n), k(_k),
-                                              b(_n/_c), c(_c), c_64(_c) {}
+                                              b(_n/_c), c(_c) {}//, c_64(_c) {}
     int i;
     __host__ __device__ 
     void set_i(const int& _i) {
@@ -56,26 +58,42 @@ struct shuffle {
     }
     //This returns long long to avoid integer overflow in intermediate
     //computation
-    __host__ __device__ 
+    // __host__ __device__ 
+    // long long f(const int& j) {
+    //     long long r = j + (long long)i * (long long)(n - 1);
+    //     if ((i - (int)c.mod(j)) <= (m - (int)c.get())) {
+    //         return r;
+    //     } else {
+    //         return r + m;
+    //     }
+    // }
+    __host__ __device__
     long long f(const int& j) {
-        long long r = j + (long long)i * (long long)(n - 1);
-        if ((i - (int)c.mod(j)) <= (m - (int)c.get())) {
+        long long r = j + i * (n - 1);
+        if ((i - (j % c)) <= (m - c)) {
             return r;
         } else {
             return r + m;
         }
     }
     
-    __host__ __device__ 
+    // __host__ __device__ 
+    // int operator()(const int& j) {
+    //     long long fij = f(j);
+    //     unsigned long long fijdivc;
+    //     unsigned long long fijmodc;
+    //     c_64.divmod(fij, fijdivc, fijmodc);
+        
+    //     int term1 = b.mod((k *((int)fijdivc)));
+    //     int term2 = ((int)fijmodc) * (int)b.get();
+    //     return term1 + term2;
+    // }
+    __host__ __device__
     int operator()(const int& j) {
         long long fij = f(j);
-        unsigned long long fijdivc;
-        unsigned long long fijmodc;
-        c_64.divmod(fij, fijdivc, fijmodc);
-        
-        int term1 = b.mod((k *((int)fijdivc)));
-        int term2 = ((int)fijmodc) * (int)b.get();
-        return term1 + term2;
+        int term_1 = (k * (fij / c)) % b;
+        int term_2 = (fij % c) * b;
+        return term_1 + term_2;
     }
 };
 
